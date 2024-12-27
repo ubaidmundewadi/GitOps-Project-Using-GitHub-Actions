63c3286cb5591fbca97ab491b7a9dd98b2f12bd5
Ubaid <mundewadiubaid@gmail.com>
2024-12-27 18:42:08 +0530

    test 5

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "vprofile-eks"
 
   cidr = "172.20.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs  = ["us-east-1a", "us-east-1b", "us-east-1c"]
 
   private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
   public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
